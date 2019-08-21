835be33025f8c621042af2d0105b259753172aff
Anker166 <andrei.k.166@mail.ru>
2019-08-21 21:56:27 +0300

    Выполнил все задания

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -1,3 +1,5 @@
+__author__ = 'Кусый Андрей Геннадьевич'
+
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
@@ -5,13 +7,19 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    
+    x =  number * 10 ** (ndigits + 1) % 10 // 1
+    rez = number * 10 ** ndigits // 1
+    
+    if x >= 5:
+        return (rez + 1) / 10 ** ndigits
+    else:
+        return rez / 10 ** ndigits
 
 
-print(my_round(2.1234567, 5))
-print(my_round(2.1999967, 5))
-print(my_round(2.9999967, 5))
-
+print(my_round(2.12345673, 4))
+print(my_round(2.19999673, 5))
+print(my_round(2.99999673, 5))
 
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
@@ -20,9 +28,17 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
-
+    
+    left_part = ticket_number // 100000 % 10 + ticket_number // 10000 % 10 + ticket_number // 1000 % 10
+    right_part = ticket_number // 100 % 10 + ticket_number // 10 % 10 + ticket_number % 10
+    
+    return left_part == right_part
+    
+    #if left_part == right_part:
+    #    return 'Счастливый'
+    #else:
+    #    return 'Несчастливый'
 
 print(lucky_ticket(123006))
-print(lucky_ticket(12321))
+print(lucky_ticket(123214))
 print(lucky_ticket(436751))

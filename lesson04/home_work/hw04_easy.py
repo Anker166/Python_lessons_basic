70f90ed7e84a87c1196aa77b7f1a25f30fbcdfa5
Anker166 <andrei.k.166@mail.ru>
2019-08-26 14:07:06 +0300

    Выполнил все задания

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -6,9 +6,18 @@
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
+origin_lst = [1, 2, 4, 0, 9, 7, 5, 3, 6]
+modified_lst = [i ** 2 for i in origin_lst]
+print(modified_lst)
+
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+
+fruits1 = ['банан', 'апельсин', 'ананас', 'киви', 'яблоко']
+fruits2 = ['апельсин', 'киви', 'маракуйя', 'арбуз']
+fruits3 = [i for i in fruits1 if i in fruits2]
+print(fruits3)
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -16,3 +25,10 @@
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+import random
+
+first_lst = [random.randint(-100, 100) for i in range(15)]
+print(first_lst)
+second_lst = [i for i in first_lst if not i % 3 and i > 0 and i % 4]
+print(second_lst)

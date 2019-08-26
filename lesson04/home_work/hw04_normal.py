ce842477347959209916e0df2f1fa75a532aff09
Anker166 <andrei.k.166@mail.ru>
2019-08-26 14:08:28 +0300

    lesson 4. выполнил все задания

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -4,6 +4,8 @@
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
 
+import re
+
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
@@ -21,6 +23,12 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
 
+pattern = r'[a-z]+'
+result = re.findall(pattern, line)
+print(result)
+
+print(f'{"*" * 50}')
+
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
@@ -28,6 +36,9 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
+
+#line_2 = "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+
 
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
@@ -45,9 +56,72 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+
+alpha_capital = [chr(i) for i in range(65, 91)]
+alpha_ascender = [chr(i) for i in range(97, 123)]
+result_alpha = []
+i = 2
+n = 0 
+while i <= len(line_2) - 2:
+    if line_2[i - 2] in alpha_ascender and line_2[i - 1] in alpha_ascender:
+        n = i
+        if i > len(line_2) - 3:
+            break
+        else:
+            while line_2[i] in alpha_capital and line_2[i+1] in alpha_capital and line_2[i+2] in alpha_capital:
+                i += 1
+                if i > len(line_2) - 3:
+                    break                
+            if line_2[n:i]:
+                result_alpha.append(line_2[n:i])
+        i += 1
+    else:
+        i += 1
+print(result_alpha)
+
+print(f'{"*" * 50}')
+
+pattern_2 = r'(?<=[a-z]{2})[A-Z]+(?=[A-Z]{2})'
+result_2 = re.findall(pattern_2, line_2)
+print(result_2)
+
+
+print(f'{"*" * 50}')
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import random
+
+n = 0
+i = 0
+string_max = ''
+
+with open('test.txt', 'w', encoding = 'UTF-8') as inf:
+    inf.write(str(random.randint(0,9)))
+
+with open('test.txt', 'a+', encoding = 'UTF-8') as inf:
+    while len(inf.read()) < 2500:
+        inf.write(str(random.randint(0,9)))
+        inf.seek(0)
+
+with open('test.txt', 'r', encoding = 'UTF-8') as inf:
+    string_1 = str(inf.read())
+    while True:
+        n = i
+        if i >= len(string_1) - 1:
+            break
+        else:
+            while string_1[i] == string_1[i + 1]:
+                i += 1
+                if i >= len(string_1) - 1:
+                    break
+            if len(string_max) < len(string_1[n:i + 1]):
+                string_max = string_1[n:i + 1]     
+        i += 1
+   
+    print(string_max)
